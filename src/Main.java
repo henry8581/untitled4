907a46bcef171a3173fe9e3d7ae448df3b5b8754
I432 <henry8581@yahoo.com.tw>
2017-11-06 14:35:23 +0800

    11/6

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String args[]) {
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+
+        double x = sc.nextDouble();
+
+        double y = sc.nextDouble();
+
+        if (x > y) {
+            System.out.println(x + ">" + y);
+
+
+        } else {
+            System.out.println(x + ">" + y);
+        }
+
+    }
+}
+
